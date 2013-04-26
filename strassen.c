960927f3b093b8c6b97061e492355d3d39960df0
Peter Birsinger <pbirsinger@berkeley.edu>
2013-04-26 01:16:28 +0000

    recursive unoptimized strassens works

--- strassen.c
+++ strassen.c
@@ -26,21 +26,17 @@ int should_run_base_case(Problem p) {
   }
 }
 
-void matrix_add(int m, int n, double *A, double *B, double *C) {
-    int i,j;
-    for (i =0; i<m; i++){
-        for (j =0; j <n; j++){
-            C[i + j*m] = A[i+j*m] + B[i+j*m];
-        }
+void matrix_add(int N, double *A, double *B, double *C) {
+    int i;
+    for (i =0 ; i < N; i++){
+        C[i] = A[i] + B[i];
     }
 }
 
-void matrix_subtract(int m, int n, double *A, double *B, double *C) {
-    int i,j;
-    for (i =0; i<m; i++){
-        for (j =0; j <n; j++){
-            C[i + j*m] = A[i+j*m] - B[i+j*m];
-        }
+void matrix_subtract(int N, double *A, double *B, double *C) {
+    int i;
+    for (i =0 ; i < N; i++){
+        C[i] = A[i] - B[i];
     }
 }
 
@@ -68,7 +64,6 @@ void strassen_multiply(int m, int k, int n, double *A, double *B, double *C) {
     int j =0;
     if (should_run_base_case(problem)){
         base_case(m, k, n, A, B, C);
-        //switch to MKL 
     }
     else{
         //assumign m,k,n even for now
@@ -98,7 +93,6 @@ void strassen_multiply(int m, int k, int n, double *A, double *B, double *C) {
         double *S5 = (double*) malloc(S_k * S_n * sizeof(double));
         double *S6 = (double*) malloc(S_k * S_n * sizeof(double));
 
-
         for (i = 0; i < T_k; i++){
             memcpy(T0 +i * T_m, A + i*m, T_m * sizeof(double));
             memcpy(T1 +i * T_m, A +(i+T_k) *m, T_m * sizeof(double));
@@ -110,28 +104,36 @@ void strassen_multiply(int m, int k, int n, double *A, double *B, double *C) {
             memcpy(T6 +i * T_m, A +T_m+ (i+T_k)*m, T_m * sizeof(double));
         }
 
-        for (i = 0; i < S_n; i++){
+        for (i = 0; i < S_n; i++) {
             memcpy(S0 +i * S_k, B + i*k, S_k * sizeof(double));
 
-            memcpy(S1 +i * S_k, B +S_k+ i*k, S_k * sizeof(double));
+            memcpy(S1 +i * S_k, B + S_k + i*k, S_k * sizeof(double));
             memcpy(S2 +i * S_k, B + (i+S_n)*k, S_k * sizeof(double));
+
             memcpy(S3 +i * S_k, B + S_k+(i+S_n)*k, S_k * sizeof(double));
-            memcpy(S4 +i * S_k, B +S_k+ (i+S_n)*k, S_k * sizeof(double));
-            memcpy(S5 +i * S_k, B+ S_k+(i+S_n)*k, S_k * sizeof(double));
+
+            memcpy(S4 +i * S_k, B + S_k+ (i+S_n)*k, S_k * sizeof(double));
+            memcpy(S5 +i * S_k, B + S_k +(i+S_n)*k, S_k * sizeof(double));
+        
         }
 
         //use daxpy for adds and subtracts 
-        //cblas_daxpy()
+        //doesn't quite work because i used z= x+y        
+        //y = a * x + y
+        // cblas_daxpy(T_m * T_k, -1, T2, 1, T0, 1);
+        // cblas_daxpy(T_m * T_k, 1, T2, 1, T0, 1);
+        // cblas_daxpy(T_m * T_k, -1, T2, 1, T0, 1);
+        // cblas_daxpy(T_m * T_k, -1, T2, 1, T0, 1);
 
-        matrix_subtract(T_m, T_k, T0, T2, T4);
-        matrix_add(T_m, T_k, T2, T6, T2);
-        matrix_subtract(T_m, T_k, T2, T1, T3);
-        matrix_add(T_m, T_k, T1, T3, T5);
+        matrix_subtract(T_m *T_k, T0, T2, T4);
+        matrix_add(T_m *T_k, T2, T6, T2);
+        matrix_subtract(T_m *T_k, T2, T1, T3);
+        matrix_add(T_m *T_k, T1, T3, T5);
 
-        matrix_subtract(S_k, S_n, S4, S2, S4);
-        matrix_add(S_k, S_n, S2, S0, S2);
-        matrix_subtract(S_k, S_n, S3, S2, S3);
-        matrix_subtract(S_k, S_n, S3, S1, S6);
+        matrix_subtract(S_k*S_n, S4, S2, S4);
+        matrix_add(S_k *S_n, S2, S0, S2);
+        matrix_subtract(S_k *S_n, S3, S2, S3);
+        matrix_subtract(S_k *S_n, S3, S1, S6);
 
         // compute Q0-6
         double *Q0 = (double*) malloc(T_m * S_n * sizeof(double));
@@ -155,9 +157,9 @@ void strassen_multiply(int m, int k, int n, double *A, double *B, double *C) {
         double *U2 = (double*) malloc(T_m * S_n * sizeof(double));
         double *U3 = (double*) malloc(T_m * S_n * sizeof(double));
 
-        matrix_add(T_m, S_n, Q0, Q3, U1);
-        matrix_add(T_m, S_n, U1, Q4, U2);
-        matrix_add(T_m, S_n, U1, Q2, U3);
+        matrix_add(T_m *S_n, Q0, Q3, U1);
+        matrix_add(T_m *S_n, U1, Q4, U2);
+        matrix_add(T_m *S_n, U1, Q2, U3);
 
         //compute C
         double *C11 = (double*) malloc(T_m * S_n * sizeof(double));
@@ -165,19 +167,26 @@ void strassen_multiply(int m, int k, int n, double *A, double *B, double *C) {
         double *C21 = (double*) malloc(T_m * S_n * sizeof(double));
         double *C22 = (double*) malloc(T_m * S_n * sizeof(double));
 
-        matrix_add(T_m, S_n, Q0, Q1, C11);
-        matrix_add(T_m, S_n, U3, Q5, C12);
-        matrix_subtract(T_m, S_n, U2, Q6, C21);
-        matrix_add(T_m, S_n, U2, Q2, C22);
+        matrix_add(T_m *S_n, Q0, Q1, C11);
+        matrix_add(T_m *S_n, U3, Q5, C12);
+        matrix_subtract(T_m *S_n, U2, Q6, C21);
+        matrix_add(T_m*S_n, U2, Q2, C22);
 
-        for (i=0; i <S_n; i++){
-            for (j=0; j <T_m; j++){
-                C[j + i*m] = C11[j+i*T_m];
-                C[j + (i+S_n)*m] = C12[j+i*T_m];
-                C[j + T_m + i*m] = C21[j+i*T_m];
-                C[j + T_m + (i+S_n)*m] = C22[j+i*T_m];
-            }
+        for (i = 0; i < S_n; i++){
+            memcpy(C +i * m, C11 + i*T_m, T_m * sizeof(double));
+            memcpy(C + (i+S_n) * m, C12 + i*T_m, T_m * sizeof(double));
+            memcpy(C +T_m+i * m, C21 + i*T_m, T_m * sizeof(double));
+            memcpy(C +T_m+(i+S_n) * m, C22 + i*T_m, T_m * sizeof(double));
         }
+
+        // for (i=0; i <S_n; i++){
+        //     for (j=0; j <T_m; j++){
+        //         C[j + i*m] = C11[j+i*T_m];
+        //         C[j + (i+S_n)*m] = C12[j+i*T_m];
+        //         C[j + T_m + i*m] = C21[j+i*T_m];
+        //         C[j + T_m + (i+S_n)*m] = C22[j+i*T_m];
+        //     }
+        // }
 
         free(T0);
         free(T1);
